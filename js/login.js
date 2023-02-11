5dbebcc8aaedd76cb41e64259f37dd33b978fc56
MostakemHossain <mostakememon123@gmail.com>
2023-02-12 00:02:24 +0600

    verify email and pass

---
+++ js/login.js
@@ -0,0 +1,21 @@
+// step 1: click event hander with submit button
+
+document.getElementById('btn-submit').addEventListener('click',function(){
+    //step-3
+    //get the email address inside the imail input field
+    const emailField= document.getElementById('email');
+    const email= emailField.value;
+
+    //step 3: get password
+    const passField= document.getElementById('password');
+    const pass= passField.value;
+
+    //step-4 verify email and password
+
+    if(email==='mostakememon123@gmail.com' && pass==='mostakem123'){
+
+    }else{
+        
+    }
+    
+})
