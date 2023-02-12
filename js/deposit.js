024eb8b42ac0c0307edeecd003ef24e69f58fef2
MostakemHossain <mostakememon123@gmail.com>
2023-02-12 20:06:20 +0600

    final-deposit

--- js/deposit.js
+++ js/deposit.js
@@ -2,11 +2,30 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
 //step-2:get the deposit amount in the input field
 const depositField=document.getElementById('input-deposit');
-const depositAmmount=depositField.value;
+const newDepositAmmountString=depositField.value;
+const newDepositAmmount=parseFloat(newDepositAmmountString);
     
   //step-3:get the current deposit total
 
   const depositTotalElement= document.getElementById('deposit-total');
-  const depositTotal= depositTotalElement.innerText;
-  depositTotalElement.innerText=depositAmmount;
+  const currentDepositTotalString= depositTotalElement.innerText;
+  const currentDepositTotal=parseFloat(currentDepositTotalString);
+
+  //step-4: add number to the total deposit
+  const Total= newDepositAmmount+currentDepositTotal;
+  depositTotalElement.innerText=Total;
+
+  //step-5:get balance current total
+
+  const balanceTotalElement= document.getElementById('balance-total');
+  const previousBalanceTotalString= balanceTotalElement.innerText;
+  const currentBalanceTotal=parseFloat(previousBalanceTotalString);
+
+//step-6: add number to the total balance
+const TotalBalance=currentBalanceTotal+newDepositAmmount;
+balanceTotalElement.innerText=TotalBalance;
+
+
+
+  depositField.value='';
 })
